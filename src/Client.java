e8f6a6b4878fa35d89c895ff5f11c9d45163543e
breyden <breyden.landberg@students.mq.edu.au>
2021-03-25 16:12:04 +1100

    worked more on client

--- src/Client.java
+++ src/Client.java
@@ -21,9 +21,9 @@ public class Client {
 
     // other fields
     private static int count; // will hold the current amount of "available" bytes from s.getInputStream()
-    private static byte[] byteArray; // will hold the current message from the server stored as bytes
-    private static char[] charArray; // will hold the current message from the server stored as chars (casted to char from the bytes in byteArray)
-    private static String inputStreamString; // the String instance in which we will store the server's commands
+    private static byte[] byteBuffer; // will hold the current message from the server stored as bytes
+    private static char[] charBuffer; // will hold the current message from the server stored as chars (casted to char from the bytes in byteArray)
+    private static String stringBuffer; // the String instance in which we will store the server's commands
 
     public static void main(String[] args) throws IOException {
         Socket s = new Socket(hostname, serverPort); // socket with host IP of 127.0.0.1 (localhost), server port of 50000
@@ -32,7 +32,7 @@ public class Client {
 
         try {
             System.out.println("sent HELO");
-            byte[] byteBuffer = HELO.getBytes();
+            byteBuffer = HELO.getBytes();
             dout.write (byteBuffer);
             dout.flush();
 
@@ -58,18 +58,18 @@ public class Client {
             System.out.println("receiving JOBN");
 
             din.skipBytes(OK.length() * 2); // skip the first two OK commands sent by server
-            int count = din.available(); // get length of byte buffer from din's number of bytes that can be read
+            count = din.available(); // get length of byte buffer from din's number of bytes that can be read
 
             byteBuffer = new byte[count];
             din.read(byteBuffer); // read from din into byte buffer
-            char[] charBuffer = new char[count];
+            charBuffer = new char[count];
 
             // cast byte array into char array
             for(int i = 0; i < count; i++) {
                 charBuffer[i] = (char)byteBuffer[i];
             }
 
-            String stringBuffer = new String(charBuffer); // cast char array into String
+            stringBuffer = new String(charBuffer); // cast char array into String
             System.out.println(stringBuffer);
 
             while(!stringBuffer.contains(NONE)) {
@@ -94,9 +94,11 @@ public class Client {
 
                 // stringBuffer = new String(charBuffer); // cast char array into String
                 // System.out.println(stringBuffer);
-            }
 
-            // make a method for the above
+                // make a method for the above
+
+                break;
+            }
 
             // map String array to Job class values
 
