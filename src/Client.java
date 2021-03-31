84c0e1094187d91c5bc17bea11e33fae5952678a
Amir Lingcoran <iamiracle.lingcoran@gmail.com>
2021-03-31 16:17:23 +1100

    Revert "Upload prac work"
    This reverts commit 76cc6545e909cdb12ede161a222565e7b168e733.

--- src/Client.java
+++ src/Client.java
@@ -23,7 +23,6 @@ public class Client {
     private static final String AUTH_username = AUTH + " " + username;
     private static final String REDY = "REDY";
     private static final String JOBN = "JOBN";
-    private static final String JCPL = "JCPL";
     private static final String SCHD = "SCHD";
     private static final String NONE = "NONE";
     private static final String QUIT = "QUIT";
@@ -37,7 +36,7 @@ public class Client {
     private static final int CHAR_BUFFER_LENGTH = 50; // will hold the current amount of "available" bytes from s.getInputStream()
 
     private static List<Server> serverList; // create server object List to store server information
-    private static File DSsystemXML = new File("/home/amir/Documents/ds-sim/src/pre-compiled/ds-system.xml"); // create file object
+    private static File DSsystemXML = new File("/home/tanayg/ds-sim/src/pre-compiled/ds-system.xml"); // create file object
 
     public static void main(String[] args) throws IOException {
         serverList = new ArrayList<>(); // initialise list of servers
@@ -62,7 +61,9 @@ public class Client {
             // replies with OK after printing
 
             readXML ();
+
             Server largestServer = getLargestServer(serverList); // get largest server
+            System.out.println("Largest Server:" + largestServer.type + " with " + largestServer.core + " cores");
 
             System.out.println("XML file successfully read. Sending REDY ...");
             byteBuffer = REDY.getBytes();
@@ -75,32 +76,47 @@ public class Client {
 
             din.skip(OK.length() * 2); // skip the first two OK commands sent by server
             charBuffer = new char[CHAR_BUFFER_LENGTH];
-            din.read(charBuffer); // read from din into charBuffer
+            din.read(charBuffer, 0, charBuffer.length); // read from din into charBuffer
             System.out.println("JOB successfully received.");
 
-            while(!(stringBuffer = String.valueOf(charBuffer)).contains(NONE)) {
+            stringBuffer = String.valueOf(charBuffer); // cast char array into String
+            System.out.println(stringBuffer);
+
+            while(!stringBuffer.contains(NONE)) {
                 fieldBuffer = stringBuffer.split(" "); // split String into array of strings (each string being a field of JOBN)
 
                 Job job = new Job(fieldBuffer); // create new Job object with data from fieldBuffer
                 job.printFields();
 
                 /* SCHEDULE JOB */
-                String scheduleString = SCHD + " " + job.id + " " + largestServer.type + " " + largestServer.id;
+                Server capable = serverList.get(1); // hardcoded for testing 
+                String scheduleString = SCHD + " " + job.id + " " + capable.type + " " + capable.id;
                 byteBuffer = scheduleString.getBytes();
                 dout.write(byteBuffer);
                 dout.flush();
 
-                // Send REDY for the next job
-                byteBuffer = REDY.getBytes();
-                dout.write(byteBuffer);
-                dout.flush();
+                // count = din.available();
 
-                // reset charBuffer & read next job
-                din.skip(OK.length()); // skip OK
-                charBuffer = new char[CHAR_BUFFER_LENGTH];
-                din.read(charBuffer); // read from din into charBuffer
-                System.out.println("New JOB successfully received.");
+                // byteBuffer = new byte[count];
+                // din.read(byteBuffer);
+
+                // charBuffer = new char[count];
+
+                // // cast byte array into char array
+                // for(int i = 0; i < count; i++) {
+                // charBuffer[i] = (char)byteBuffer[i];
+                // }
+
+                // stringBuffer = new String(charBuffer); // cast char array into String
+                // System.out.println(stringBuffer);
+
+                // *make a method for the above
+
+                // for testing
+                break; 
             }
+
+            // map String array to Job class values
 
             byteBuffer = QUIT.getBytes();
             dout.write(byteBuffer);
@@ -132,26 +148,26 @@ public class Client {
             doc.getDocumentElement().normalize();
             NodeList servers = doc.getElementsByTagName("server");
 
-            for (int i = 0; i < servers.getLength(); i++) { // loop through xml file and input data into appropriate variables
+            for (int i = 0; i < servers.getLength(); i++) { // loop through xml file and input data into appropriate
+                // variables
                 Element server = (Element) servers.item(i);
-                for (int j = 0; j < Integer.parseInt(server.getAttribute("limit")); j++) {
-                    String type = server.getAttribute("type");
-                    int limit = Integer.parseInt(server.getAttribute("limit"));
-                    int bootupTime = Integer.parseInt(server.getAttribute("bootupTime"));
-                    float hourlyRate = Float.parseFloat(server.getAttribute("hourlyRate"));
-                    int core = Integer.parseInt(server.getAttribute("coreCount"));
-                    int memory = Integer.parseInt(server.getAttribute("memory"));
-                    int disk = Integer.parseInt(server.getAttribute("disk"));
+                String type = server.getAttribute("type");
+                int limit = Integer.parseInt(server.getAttribute("limit"));
+                int bootupTime = Integer.parseInt(server.getAttribute("bootupTime"));
+                float hourlyRate = Float.parseFloat(server.getAttribute("hourlyRate"));
+                int core = Integer.parseInt(server.getAttribute("coreCount"));
+                int memory = Integer.parseInt(server.getAttribute("memory"));
+                int disk = Integer.parseInt(server.getAttribute("disk"));
 
-                    Server dss = new Server(j, type, limit, bootupTime, hourlyRate, core, memory, disk); // create server
-                    // object we read
-                    // from xml
-                    serverList.add(dss); // add server object to ServerList
+                Server dss = new Server(i, type, limit, bootupTime, hourlyRate, core, memory, disk); // create server
+                // object we read
+                // from xml
+                serverList.add(dss); // add server object to ServerList
 
-                    // print out the server information we read from ds-system.xml
-                    System.out.printf("%s %s %s %s %s %s %s %s", dss.id, dss.type, dss.limit, dss.bootUpTime, dss.hourlyRate, dss.core, dss.memory, dss.disk);
-                    System.out.println();
-                }
+                // print out the server information we read from ds-system.xml
+                System.out.printf("'%s %s %s %s %s %s %s", type, limit, bootupTime, hourlyRate, core, memory, disk);
+                System.out.println();
+
             }
 
         } catch (Exception e) {
@@ -161,7 +177,8 @@ public class Client {
     }
 
     public static Server getLargestServer(List<Server> s) {
-        Server largestServer = s.get(0);
+        Server largestServer = new Server(s.get(0).id, s.get(0).type, s.get(0).limit, s.get(0).bootUpTime, 
+                                    s.get(0).hourlyRate, s.get(0).core, s.get(0).memory, s.get(0).disk);
             
         for (int i = 1; i < s.size(); i++) {
             if (s.get(i).core > largestServer.core) {
